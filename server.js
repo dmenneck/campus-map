2c6981a2ad7e25fe640bbc76600612928162745b
Dirk Mennecke <dmenneck@smail.uni-koeln.de>
2020-08-06 17:42:54 +0200

    add

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-var PORT = process.env.PORT || 5000;
-var express = require("express");
-var app = express();
+const { createServer } = require("http");
+const express = require("express");
+const compression = require("compression");
+const morgan = require("morgan");
+const path = require("path");
 
-var http = require("http");
-var server = http.Server(app);
+const normalizePort = (port) => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000);
 
-app.use(express.static("client"));
+const app = express();
+const dev = app.get("env") !== "production";
 
-server.listen(PORT, function () {
-  console.log("server running");
-});
+if (!dev) {
+  app.disable("x-powered-by");
+  app.use(compression());
+  app.use(morgan("common"));
 
-var io = require("socket.io")(server);
+  app.use(express.static(path.resolve(__dirname, "build")));
 
-io.on("connection", function (socket) {
-  socket.on("message", function (msg) {
-    io.emit("message", msg);
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
+}
+
+if (dev) {
+  app.use(morgan("dev"));
+}
+
+const server = createServer(app);
+
+server.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log("Server started!");
 });
